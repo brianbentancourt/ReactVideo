8a331b8424ee7aa1fe60f52a32c9c4c1ff78760a
brianbentancourt <brianbentancourt9@gmail.com>
2018-07-20 20:47:16 -0300

    creando componentes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // reactDom renderiza componentes y elementos de react
-
+import { render } from 'react-dom'; // reactDom renderiza componentes y elementos de react
+import Media from './src/playlist/components/media'
 const app = document.getElementById('app')
 
-const holaMundo = <h1>Hola mundo! </h1>;
+//const holaMundo = <h1>Hola mundo! </h1>;
 // ReactDOM.render(que voy a renderizar, donde lo haré)
-ReactDOM.render(holaMundo,app);
+// al renderizar, react sabe que contiene < /> y por eso sabe que es un componente, por ejemplo ReactDOM.render(<Media />,app);
+render(<Media />,app);
 
---
+++ src/playlist/components/media.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Media extends Component{
+	render(){
+		return (
+			<div>
+				<img
+					src=""
+					alt=""
+					width={260}
+					height={160}
+				/>
+				<h3> ¿Por que aprender react?</h3>
+				<p>Brian Bentancourt</p>
+			</div>
+			)
+	}
+}
+
+export default Media;
