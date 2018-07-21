76d379a5221a171eba5720dbac160c86b0629fcb
brianbentancourt <brianbentancourt9@gmail.com>
2018-07-20 22:22:07 -0300

    creando componentes

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 
 class Media extends Component{
 	render(){
+		const styles = {
+			container:{ 
+				color: '#44546b',
+				cursor: 'pointer',
+				width: 260,
+				border: '1px solid Red'
+			}
+		}
 		return (
-			<div>
+			<div style={styles.container}>
 				<img
 					src=""
 					alt=""
 					width={260}
 					height={160}
 				/>
-				<h3> ¿Por que aprender react?</h3>
+				<h3>¿Por que aprender react?</h3>
 				<p>Brian Bentancourt</p>
 			</div>
 			)
