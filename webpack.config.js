9c2e8506dba70317220d1e6e7fbf550b0237143c
brianbentancourt <brianbentancourt9@gmail.com>
2018-07-31 19:40:44 -0300

    Estructura del proyecto con Containers y UI components: layout de nuestro sitio web

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env) => {
   return {
 
     entry: {
-      "home": path.resolve(__dirname, 'srs/entries/home.js'),
+      "home": path.resolve(__dirname, 'src/entries/home.js'),
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
