dc3a62db9941da10fa0c2d1e64303debcc037db9
brianbentancourt <brianbentancourt9@gmail.com>
2018-07-31 21:06:59 -0300

    Estructura del proyecto con Containers y UI components: Categorías

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom'; // reactDom renderiza componentes y elementos de react
 //import Categories from './src/playlist/components/categories'
 import Home from '../pages/containers/home';
-//import data from './src/api.json'
+import data from '../api.json';
 
 const app = document.getElementById('app');
 
 // ReactDOM.render(que voy a renderizar, donde lo haré)
 // al renderizar, react sabe que contiene < /> y por eso sabe que es un componente, por ejemplo ReactDOM.render(<Media />,app);
 
-render(<Home />,app);
+render(<Home data={data} />, app);
 
 
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,37 +1,15 @@
 import React, { Component} from 'react';
 import Media from './media.js'
 import './playlist.css';
-import Play from '../../icons/components/play'
-import Pause from '../../icons/components/pause'
-import Volume from '../../icons/components/volume'
-import FullScreen from '../../icons/components/fullscreen'
 
 class Playlist extends Component{
 	render(){
-		const playlist = this.props.playlist;
-
 		return(
 			<div className="Playlist">
-				<Play 
-					size={50}
-					color="blue"
-				/>
-				<Pause 
-					size={50}
-					color="orange"
-				/>
-				<Volume 
-					size={50}
-					color="green"
-				/>
-				<FullScreen 
-					size={45}
-					color="grey"
-				/>
 				<h3> {this.props.title}</h3>
 				<p> {this.props.description}</p>
 					{
-						playlist.map((item)=> {
+						this.props.playlist.map((item)=> {
 							return <Media {...item} key={item.id} />
 						})
 					}
