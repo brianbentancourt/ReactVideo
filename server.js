54c2068c003846b50071aedad039b37fefd7174e
brianbentancourt <brianbentancourt9@gmail.com>
2018-08-23 22:08:29 -0300

    servidor funcionando

--- server.js
+++ server.js
@@ -1,31 +1,53 @@
+/*
 import express from 'express';
 import Home from './dist/ssr/home';
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import data from './src/api.json';
 import Base from './src/pages/components/base';
+*/
+let express = require('express');
+//let Home = require('./dist/ssr/home');
+//let React = require('react');
+//let { renderToStaticMarkup } = require('react-dom/server');
+let data = require('./src/api.json');
+//let Base = require('./src/pages/components/base');
 
 const app = express();
 
-app.use(express.static('dist'))
-app.use('/images', express.static('images'))
+//app.use(express.static('dist'))
+//app.use('/images', express.static('images'))
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 9000;
+
+app.get('/dist/css/home', (req, res) => {
+  res.sendFile('./dist/css/home.css', {root: __dirname });
+})
+
+app.get('/dist/js/home', (req, res) => {
+  res.sendFile('./dist/js/home.js', {root: __dirname });
+})
+
+app.get('/images/logo.png', (req, res) => {
+  res.sendFile('./images/logo.png', {root: __dirname });
+})
 
 app.get('/', (req, res) => {
   // fetch('dame datos').then((datos)=>{
 
   // })
-  res.write(renderToStaticMarkup(
-    <Base
-      title="Platzi Video"
-      css="css/home.css"
-      js="js/home.js"
-    >
-      <Home data={data} />
-    </Base>
-  ));
-  res.end();
+  res.sendFile('./index.html', {root: __dirname });
+
+  // res.write(renderToStaticMarkup(
+  //   <Base
+  //     title="Platzi Video"
+  //     css="css/home.css"
+  //     js="js/home.js"
+  //   >
+  //     <Home data={data} />
+  //   </Base>
+  // ));
+  // res.end();
 })
 
 
--- src/utilities/browser.js
+++ src/utilities/browser.js
@@ -1,35 +1,85 @@
-const chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-const firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-const opera = navigator.userAgent.toLowerCase().indexOf('opera');
-const ie = navigator.userAgent.indexOf("MSIE") > -1 ;
-const edge = navigator.userAgent.indexOf("Edge") > -1;
+// const chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+// const firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+// const opera = navigator.userAgent.toLowerCase().indexOf('opera');
+// const ie = navigator.userAgent.indexOf("MSIE") > -1 ;
+// const edge = navigator.userAgent.indexOf("Edge") > -1;
+
+
+const { detect } = require('detect-browser');
+const browser = detect();
 
 export function IsFullScreen(){
-	if(chrome || edge || opera){
-		return document.webkitIsFullScreen;
-	}else if(firefox){
-		return document.mozFullScreen;
-	}else if(ie){
-		//TODO
+	switch (browser && browser.name) {
+	  case 'chrome':
+			return document.webkitIsFullScreen;
+	  case 'firefox':
+	    return document.mozFullScreen;
+	  case 'edge':
+	    return document.webkitIsFullScreen;
+	  default:
+	    console.log('not supported');
 	}
 }
 
 export function RequestFullScreen(element){
-	if(chrome || edge || opera){
-		element.player.webkitRequestFullscreen();
-	}else if(firefox){
-		element.player.mozRequestFullScreen();
-	}else if(ie){
-		element.player.msRequestFullscreen() ;
+	switch (browser && browser.name) {
+	  case 'chrome':
+			element.player.webkitRequestFullscreen();
+			break;
+	  case 'firefox':
+	    element.player.mozRequestFullScreen();
+			break;
+	  case 'edge':
+	    element.player.webkitRequestFullscreen();
+			break;
+	  default:
+	    element.player.webkitRequestFullscreen();
 	}
 }
 
 export function ExitFullScreen(){
-	if(chrome || edge || opera){
-		document.webkitExitFullscreen();
-	}else if(firefox){
-		document.mozCancelFullScreen();
-	}else if(ie){
-		document.msExitFullscreen();
+	switch (browser && browser.name) {
+	  case 'chrome':
+			document.webkitExitFullscreen();
+			break;
+	  case 'firefox':
+	    document.mozCancelFullScreen();
+			break;
+	  case 'edge':
+	    document.webkitExitFullscreen();
+			break;
+	  default:
+	    document.msExitFullscreen();
 	}
 }
+
+
+// export function IsFullScreen(){
+// 	if(chrome || edge || opera){
+// 		return document.webkitIsFullScreen;
+// 	}else if(firefox){
+// 		return document.mozFullScreen;
+// 	}else if(ie){
+// 		//TODO
+// 	}
+// }
+
+// export function RequestFullScreen(element){
+// 	if(chrome || edge || opera){
+// 		element.player.webkitRequestFullscreen();
+// 	}else if(firefox){
+// 		element.player.mozRequestFullScreen();
+// 	}else if(ie){
+// 		element.player.msRequestFullscreen() ;
+// 	}
+// }
+
+// export function ExitFullScreen(){
+// 	if(chrome || edge || opera){
+// 		document.webkitExitFullscreen();
+// 	}else if(firefox){
+// 		document.mozCancelFullScreen();
+// 	}else if(ie){
+// 		document.msExitFullscreen();
+// 	}
+// }
